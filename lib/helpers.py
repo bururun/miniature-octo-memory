8a4ded837a21a491050437f43132b953a84dd40f
bururun <bururun@users.noreply.github.com>
2025-12-08 23:39:08 +0200

    remove: add logging

--- lib/helpers.py
+++ lib/helpers.py
@@ -7,3 +7,14 @@ def helper_function_3(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_8(x):
+    """Helper function for iteration 8."""
+    return x * 8
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
